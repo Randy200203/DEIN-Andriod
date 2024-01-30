d3fbef64635e5a2b6d23bc070452f96838e09fd5
Randy <rachana@educacion.navarra.es>
2024-01-30 19:51:50 +0100

    light & dark

---
+++ Light__Dark_Randy/settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Light_&_Dark_Randy"
+include(":app")
+ 
