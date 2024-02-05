803d7264e69ee17a426f55f656bc26ead807d68a
Randy <rachana@educacion.navarra.es>
2024-02-05 17:39:35 +0100

    Menu Pop up hecho

---
+++ Randy_Menu_Recursos_Menu/settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Randy_Menu_Recursos_Menu"
+include(":app")
+ 
