ef07d7e9a11e25f782931ae28f89d6fb3a0d1341
Randy <rachana@educacion.navarra.es>
2024-03-06 20:29:47 +0100

    recycle view try

---
+++ app/src/main/java/com/example/ejemplo_recycleview/MainActivity.kt
@@ -0,0 +1,34 @@
+package com.example.ejemplo_recycleview
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var adapter: MyAdapter
+    private val myList = ArrayList<String>()
+    private lateinit var recyclerView : RecyclerView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        adapter = MyAdapter(myList)
+        recyclerView.layoutManager = LinearLayoutManager(this)
+        recyclerView.adapter = adapter
+
+        val addButton: Button = findViewById(R.id.addButton)
+        addButton.setOnClickListener {
+            addElement()
+        }
+    }
+
+    private fun addElement() {
+        val newItem = "Item ${myList.size + 1}"
+        myList.add(newItem)
+        adapter.notifyItemInserted(myList.size - 1)
+    }
+}
---
+++ app/src/main/java/com/example/ejemplo_recycleview/MyAdapter.kt
@@ -0,0 +1,27 @@
+package com.example.ejemplo_recycleview
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+
+class MyAdapter(private val myList: ArrayList<String>) : RecyclerView.Adapter<MyAdapter.ViewHolder>() {
+
+    class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        val textView: TextView = view.findViewById(R.id.textView)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.item_layout, parent, false)
+        return ViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.textView.text = myList[position]
+    }
+
+    override fun getItemCount(): Int {
+        return myList.size
+    }
+}
